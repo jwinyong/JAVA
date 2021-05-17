bf6372abb55e2681ea4f1a762562c0602f09ef85
PARK-JINHYEONG <84114159+jwinyong@users.noreply.github.com>
2021-05-17 23:13:56 +0900

    [구구단 3단 예제] Baseline (ver 1.0)
    for 문을 이용하여 만든 구구단 3단 코드입니다.

---
+++ samDan.java
@@ -0,0 +1,13 @@
+package portfolio;
+
+public class samDan {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 10; i++) {
+			
+			System.out.println("3 x " + i + " = " + (3 * i));
+		}
+	}
+
+}
