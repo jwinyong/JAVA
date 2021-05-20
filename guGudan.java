3d43a81360922ae8f5939c76bc0bd0a30e0c6cb0
PARK-JINHYEONG <84114159+jwinyong@users.noreply.github.com>
2021-05-20 21:06:28 +0900

    [구구단 예제] 모듈 함수화 (ver 1.1)
    Baseline 에서 가독성과 재사용성을 높이기 위해 모듈을 함수화하여 수정해 보았습니다.

--- guGudan.java
+++ guGudan.java
@@ -1,8 +1,10 @@
 package portfolio;
 
-public class guGudan {
+public class guGudan 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// 2단부터 9단까지 구구단 출력하기
 
 		// 1. 2-9까지 도는 for문 만들기
@@ -12,20 +14,28 @@ public class guGudan {
 		// 3. 위 두 변수 값을 곱해서 출력!
 
 		int dan = 1;
-		int mul = 0;
 
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < 8; i++) 
+		{
 			dan++;
-
+		
 			System.out.println("--- " + dan + "단 ---");
+			
+			printDan(dan);
 
-			for (int k = 0; k < 9; k++) {
-				mul++;
-
-				System.out.println(dan + " x " + mul + " = " + (dan * mul));
-			}
-			mul = 0;
 		}
 	}
-
+	
+	
+	public static void printDan(int dan) 
+	{
+		int mul = 1;
+		
+		for (int k = 0; k < 9; k++) 
+		{
+			System.out.println(dan + " x " + mul + " = " + (dan * mul));
+			mul++;
+		}
+		mul = 0;
+	}
 }
